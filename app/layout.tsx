2547a65fcd55ca5069f0638490a9f762cf5a94d9
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-22 23:31:24 +0530

    added logo images in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              <img
+                src="/printforge-logo.svg"
+                alt="PrintForge Logo"
+                className="w-[200px] h-auto hidden md:block"
+              />
+              {/* Mobile logo */}
+            </div>
+            <ul className="flex items-center gap-2.5">
+              <p>3D Models</p>
+              <p>About</p>
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
