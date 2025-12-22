bd31908673ad020dc7e72001c5622ad0d1ade591
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-22 23:32:31 +0530

    added mobile view logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,18 @@ export default function RootLayout({
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
             <div className="relative">
+              {/* Desktop logo */}
               <img
                 src="/printforge-logo.svg"
                 alt="PrintForge Logo"
                 className="w-[200px] h-auto hidden md:block"
               />
               {/* Mobile logo */}
+              <img
+                src="/printforge-logo-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden"
+              />
             </div>
             <ul className="flex items-center gap-2.5">
               <p>3D Models</p>
