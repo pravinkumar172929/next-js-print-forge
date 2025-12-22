2acc01db7852e2720d195162f91d5d9d9d856dc3
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-22 22:52:00 +0530

    added homepage image and headings

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Welcome to setup</h1>;
+  return (
+    <main>
+      <h1>Discover what's possible with 3D printing</h1>
+      <p>
+        Join our community of creators and explore a vast library of
+        user-submitted models.
+      </p>
+      <button>Browse Models</button>
+      <img src="home-page-hero-image.png" />
+    </main>
+  );
 }
