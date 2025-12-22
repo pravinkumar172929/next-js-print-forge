9dc415d7da3796ace5f3aa4f1b70f847c1b54ade
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 01:23:48 +0530

    added Link inside li as they are only direct children of ul

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,8 +45,12 @@ export default function RootLayout({
               </div>
             </Link>
             <ul className="flex items-center gap-2.5">
-              <p>3D Models</p>
-              <p>About</p>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/3d-models">3D Models</Link>
+              </li>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/about">About</Link>
+              </li>
             </ul>
           </nav>
         </header>
