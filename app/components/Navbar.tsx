eaffb95c1cceddb15b6d61af0e313cf7263f2a22
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 11:01:26 +0530

    moved navbar in components and imported in layout

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import PFLogo from "@/public/printforge-logo.svg";
+import PFLogoIcon from "@/public/printforge-logo-icon.svg";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="w-full bg-white">
+      <nav className="flex justify-between px-6 py-4">
+        <Link href="/">
+          <div className="relative">
+            {/* Desktop logo */}
+            <img
+              src={PFLogo.src}
+              alt="PrintForge Logo"
+              className="w-[200px] h-auto hidden md:block"
+            />
+            {/* Mobile logo */}
+            <img
+              src={PFLogoIcon.src}
+              alt="PrintForge Logo"
+              className="w-[40px] h-auto block md:hidden"
+            />
+          </div>
+        </Link>
+        <ul className="flex items-center gap-2.5">
+          <li className="text-sm uppercase cursor-pointer">
+            <Link href="/3d-models">3D Models</Link>
+          </li>
+          <li className="text-sm uppercase cursor-pointer">
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "./globals.css";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
-import PFLogo from "@/public/printforge-logo.svg";
-import PFLogoIcon from "@/public/printforge-logo-icon.svg";
-import Link from "next/link";
 import type { RootLayoutPros } from "./types";
+import Navbar from "./components/Navbar";
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -23,34 +21,7 @@ export default function RootLayout({ children }: RootLayoutPros) {
       <body
         className={`${albertSans.className} ${montserratAlternates.variable}`}
       >
-        <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4">
-            <Link href="/">
-              <div className="relative">
-                {/* Desktop logo */}
-                <img
-                  src={PFLogo.src}
-                  alt="PrintForge Logo"
-                  className="w-[200px] h-auto hidden md:block"
-                />
-                {/* Mobile logo */}
-                <img
-                  src={PFLogoIcon.src}
-                  alt="PrintForge Logo"
-                  className="w-[40px] h-auto block md:hidden"
-                />
-              </div>
-            </Link>
-            <ul className="flex items-center gap-2.5">
-              <li className="text-sm uppercase cursor-pointer">
-                <Link href="/3d-models">3D Models</Link>
-              </li>
-              <li className="text-sm uppercase cursor-pointer">
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Navbar />
         {children}
       </body>
     </html>
