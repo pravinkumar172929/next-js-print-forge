6f6e69c3dfc5071bc74bc0921a35e84b4f89adab
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 12:39:28 +0530

    async function and type throw error to satisfy ts

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -1,5 +1,14 @@
 import modelsData from "../data/models.json";
+import type { Model } from "../types";
 
-export async function getAllModels() {
+export async function getAllModels(): Promise<Model[]> {
   return modelsData;
+}
+
+export async function getModelById(id: string | number): Promise<Model> {
+  const foundModel = modelsData.find((model) => model.id === id);
+  if (!foundModel) {
+    throw new Error("model not found");
+  }
+  return foundModel;
 }
