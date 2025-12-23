ba54f19cbf31579e4538b23a544053dd1ab41331
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 10:48:31 +0530

    getAllModels using awit and making ModelLidt async

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,3 +1,6 @@
-export default function ModelsList() {
+import { getAllModels } from "../lib/models";
+export default async function ModelsList() {
+  const models = await getAllModels();
+  console.log(models);
   return <h1>models List</h1>;
 }
