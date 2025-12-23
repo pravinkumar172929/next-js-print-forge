0353366229623dcdb98ef1775aeec8e1eb313826
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 14:19:10 +0530

    by making function async logged CategoryName

--- app/3d-models/categories/[categoryName]/page.tsx
+++ app/3d-models/categories/[categoryName]/page.tsx
@@ -1,3 +1,9 @@
-export default function CategoryPage() {
-  return <p>CategoryPage</p>;
+export default async function CategoryPage({ params }) {
+  const { categoryName } = await params;
+  return (
+    <>
+      <h1>CategoryPage</h1>
+      <p>{categoryName}</p>
+    </>
+  );
 }
