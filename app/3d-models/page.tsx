6734d4b150dcaffaa3cc17f724fcaf8158b76df3
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 11:07:24 +0530

    3d-model page styling

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -4,10 +4,17 @@ export default async function ModelsList() {
   const models = await getAllModels();
   console.log(models);
   return (
-    <div>
-      {models.map((model: Model) => (
-        <h1 key={model.id}>{model.name}</h1>
-      ))}
+    <div className="container px-4 py-8 mx-auto">
+      <h1 className="mb-8 text-3xl font-bold">All Models</h1>
+      <div
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        role="region"
+        aria-label="3D Models Gallery"
+      >
+        {models.map((model: Model) => (
+          <h1 key={model.id}>{model.name}</h1>
+        ))}
+      </div>
     </div>
   );
 }
