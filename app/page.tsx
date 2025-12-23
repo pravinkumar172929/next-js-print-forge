dc3ef161fdece1f4c15a40f14b68445aed3d6039
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 11:35:43 +0530

    replaced button with Link in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import heroImage from "@/public/home-page-hero-image.png";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
@@ -16,12 +18,12 @@ export default function Home() {
           </p>
 
           <div className="flex gap-4">
-            <button
+            <Link
               href="/3d-models"
               className="px-6 py-3 text-black transition duration-100 bg-white border-2 border-black hover:bg-black hover:text-white"
             >
               Browse Models
-            </button>
+            </Link>
           </div>
         </div>
         <img src={heroImage.src} width={350} />
