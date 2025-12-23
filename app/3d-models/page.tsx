a101d5cfb4a50ac9b17a041f1dc5c209582424d2
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-12-23 22:28:33 +0530

    added search box

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -4,5 +4,16 @@ import { getAllModels } from "../lib/models";
 export default async function ModelsList() {
   const models = await getAllModels();
 
-  return <ModelsGrid models={models} title="All Models" />;
+  return (
+    <>
+      <form className="w-full px-5 md:px-0 md:max-w-xl">
+        <input
+          type="text"
+          name="query"
+          className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
+        />
+      </form>
+      <ModelsGrid models={models} title="All Models" />
+    </>
+  );
 }
