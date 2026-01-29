d78102afe5e1925c33ba8590f536601dd856920a
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-01-29 14:28:42 +0530

    underlined on active link

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -6,7 +6,7 @@ export default function NavLink({ href, children, isActive }: NavLinkProps) {
     <li className="text-sm uppercase">
       <Link
         className={`px-4 py-2  transition-colors rounded-md cursor-pointer hover:text-orange-accent ${
-          isActive ? "text-orange-accent" : "text-gray-700"
+          isActive ? "text-orange-accent underline" : "text-gray-700"
         }`}
         href={href}
       >
